3e921f02b19e2be0313092b7cb2dde83cd378795
Vladimir Romashchenko <eaglesemanation@gmail.com>
2021-05-05 21:49:05 +0300

    fix: schema is not immediately removed
    Package replacing schema obj_robot was immediately removed because of
    typo

--- postgres-robots/postgres/02_add-package-emulating-schemas.sql
+++ postgres-robots/postgres/02_add-package-emulating-schemas.sql
@@ -6,6 +6,6 @@ DROP SCHEMA IF EXISTS obj_robot;
 CREATE SCHEMA obj_robot AUTHORIZATION postgres;
 COMMENT ON SCHEMA obj_robot IS 'Migrated obj_robot package';
 
-DROP SCHEMA IF EXISTS obj_robot;
+DROP SCHEMA IF EXISTS obj_rpart;
 CREATE SCHEMA obj_rpart AUTHORIZATION postgres;
 COMMENT ON SCHEMA obj_rpart IS 'Migrated obj_rpart package';
