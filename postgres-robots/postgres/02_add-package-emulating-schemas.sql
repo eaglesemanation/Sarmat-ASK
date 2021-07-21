de87f18943f7f6a593d90c93d709b6cc88b67a4e
Vladimir Romashchenko <eaglesemanation@gmail.com>
2021-07-21 15:50:35 -0400

    fix(package_emulation): Drop cascade
    Added cascade to all table drops on init

--- postgres-robots/postgres/02_add-package-emulating-schemas.sql
+++ postgres-robots/postgres/02_add-package-emulating-schemas.sql
@@ -1,47 +1,47 @@
-DROP SCHEMA IF EXISTS helpers;
+DROP SCHEMA IF EXISTS helpers CASCADE;
 CREATE SCHEMA helpers AUTHORIZATION postgres;
 COMMENT ON SCHEMA helpers IS 'Helper functions/procedures that assist migration from Oracle';
 
-DROP SCHEMA IF EXISTS service;
+DROP SCHEMA IF EXISTS service CASCADE;
 CREATE SCHEMA service AUTHORIZATION postgres;
 COMMENT ON SCHEMA service IS 'Migrated service package';
 
-DROP SCHEMA IF EXISTS trigger;
+DROP SCHEMA IF EXISTS trigger CASCADE;
 CREATE SCHEMA trigger AUTHORIZATION postgres;
 COMMENT ON SCHEMA trigger IS 'Migrated inline funtions/procedures for triggers';
 
-DROP SCHEMA IF EXISTS obj_robot;
+DROP SCHEMA IF EXISTS obj_robot CASCADE;
 CREATE SCHEMA obj_robot AUTHORIZATION postgres;
 COMMENT ON SCHEMA obj_robot IS 'Migrated obj_robot package';
 
-DROP SCHEMA IF EXISTS obj_rpart;
+DROP SCHEMA IF EXISTS obj_rpart CASCADE;
 CREATE SCHEMA obj_rpart AUTHORIZATION postgres;
 COMMENT ON SCHEMA obj_rpart IS 'Migrated obj_rpart package';
 
-DROP SCHEMA IF EXISTS obj_ask;
+DROP SCHEMA IF EXISTS obj_ask CASCADE;
 CREATE SCHEMA obj_ask AUTHORIZATION postgres;
 COMMENT ON SCHEMA obj_ask IS 'Migrated obj_ask package';
 
-DROP SCHEMA IF EXISTS obj_cmd_order;
+DROP SCHEMA IF EXISTS obj_cmd_order CASCADE;
 CREATE SCHEMA obj_cmd_order AUTHORIZATION postgres;
 COMMENT ON SCHEMA obj_cmd_order IS 'Migrated obj_cmd_order package';
 
-DROP SCHEMA IF EXISTS obj_cmd_gas;
+DROP SCHEMA IF EXISTS obj_cmd_gas CASCADE;
 CREATE SCHEMA obj_cmd_gas AUTHORIZATION postgres;
 COMMENT ON SCHEMA obj_cmd_gas IS 'Migrated obj_cmd_gas package';
 
-DROP SCHEMA IF EXISTS obj_doc_expense;
+DROP SCHEMA IF EXISTS obj_doc_expense CASCADE;
 CREATE SCHEMA obj_doc_expense AUTHORIZATION postgres;
 COMMENT ON SCHEMA obj_doc_expense IS 'Migrated obj_doc_expense package';
 
-DROP SCHEMA IF EXISTS extend;
+DROP SCHEMA IF EXISTS extend CASCADE;
 CREATE SCHEMA extend AUTHORIZATION postgres;
 COMMENT ON SCHEMA extend IS 'Migrated extend package';
 
-DROP SCHEMA IF EXISTS api;
+DROP SCHEMA IF EXISTS api CASCADE;
 CREATE SCHEMA api AUTHORIZATION postgres;
 COMMENT ON SCHEMA api IS 'Migrated api package';
 
-DROP SCHEMA IF EXISTS emu;
+DROP SCHEMA IF EXISTS emu CASCADE;
 CREATE SCHEMA emu AUTHORIZATION postgres;
 COMMENT ON SCHEMA emu IS 'Migrated emu package';
