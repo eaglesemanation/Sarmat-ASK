de340b4f9fdbae439ec673cf54f313f3211a9bea
Vladimir Romashchenko <eaglesemanation@gmail.com>
2021-08-01 18:40:29 -0400

    fix(dump): Support importing partially configured dump

--- postgres-robots/postgres/00_restore-from-dump.sh
+++ postgres-robots/postgres/00_restore-from-dump.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 
-pg_restore -d "$POSTGRES_DB" -U "$POSTGRES_USER" -n public "$(dirname $0)/pgree"
+#pg_restore -d "$POSTGRES_DB" -U "$POSTGRES_USER" -n public "$(dirname $0)/pgree"
+psql -v ON_ERROR_STOP=1 -d "$POSTGRES_DB" -U "$POSTGRES_USER" -f "$(dirname $0)/pgree"
--- postgres-robots/postgres/01_add-extensions.sql
+++ postgres-robots/postgres/01_add-extensions.sql
@@ -1,9 +1,11 @@
 SET client_encoding = 'UTF8';
 
+DROP EXTENSION IF EXISTS adminpack;
 CREATE EXTENSION adminpack
     SCHEMA pg_catalog
     VERSION "1.1";
 
+DROP EXTENSION IF EXISTS plpgsql_check;
 CREATE EXTENSION plpgsql_check
     SCHEMA pg_catalog
     VERSION "1.17";
